9cbb676c7b6167d5e8ba34abfa22f3f087bf1a3a
juanpaluna1234 <juanpaluna1234@gmail.com>
2022-08-22 20:49:55 -0500

    Cambios

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,16 +1,14 @@
-from email import header
-from typing import Union
 import requests
 from pydantic import BaseModel
 from fastapi import FastAPI
-
 app = FastAPI()
+
 
 class Item(BaseModel):
     name: str
     price: float
-    is_offer:bool
-    
+    is_offer: bool
+
 
 @app.get("/")
 def read_root():
@@ -18,30 +16,32 @@ def read_root():
     response = requests.get(url, {}, timeout=5)
     return {"items": response.json()}
 
+
 @app.get("/items/{item_id}")
-def read_root(item_id:int):
+def fetch_root(item_id: int):
     url = 'https://6303e5400de3cd918b3fde59.mockapi.io/items'
     response = requests.get(url+'/'+str(item_id), timeout=5)
     return {"item": response.json()}
 
 
 @app.put("/itemsPut/{item_id}")
-def update_item(item_id:int,item:Item):
-    headers = {'Content-type': 'application/json', 'Accept': 'Application/json'}
+def update_item(item_id: int, item: Item):
+    header = {'Content-type': 'application/json', 'Accept': 'Application/json'}
     url = 'https://6303e5400de3cd918b3fde59.mockapi.io/items'
-    requests.put(url+"/"+str(item_id), item.json(), headers=headers)
-    r = requests.get(url, {}, timeout=5)
-    return  "Se modifico correctamente" 
+    requests.put(url+"/"+str(item_id), item.json(), headers=header)
+    return "Se modifico correctamente"
+
 
 @app.post("/itemsPost/")
-def add_item(item:Item):
-    headers = {'Content-type': 'application/json', 'Accept': 'Application/json'}
+def add_item(item: Item):
+    header = {'Content-type': 'application/json', 'Accept': 'Application/json'}
     url = 'https://6303e5400de3cd918b3fde59.mockapi.io/items'
-    requests.post(url, item.json(), headers=headers)
+    requests.post(url, item.json(), headers=header)
     return "Se agrego correctamente"
+
 
 @app.delete("/delete/{item_id}")
 def delet_item(item_id):
     url = 'https://6303e5400de3cd918b3fde59.mockapi.io/items'
-    response = requests.delete(url+"/"+str(item_id), timeout=5)
+    requests.delete(url+"/"+str(item_id), timeout=5)
     return "Se borro correctamente"
